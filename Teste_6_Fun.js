65bd5c9956fb5798af87f89b0c80e0ea4eee7df8
Edgar <edxgar48@gmail.com>
2018-11-06 16:51:06 -0200

    trabalhado apenas na planilha

---
+++ Teste_6_Fun.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const fs = require('fs');
+const rl = readline.createInterface({
+
+    input: fs.createReadStream(`D_LOTFAC.HTM`)
+
+
+});
+
+rl.on(`line`,(line) => {
+    console.log('Line from file:',line);
+
+
+});
+rl.on(`close`, () => {
+    console.log(`acabou`);
+    
+});
+
+
+
+
+//request('http://loterias.caixa.gov.br/wps/portal/loterias/landing/lotofacil/', function (err,res,body){
+//    if (!err && res.statusCode === 200) {
+//            console.log(body)
+
+
+////    }
+//})
