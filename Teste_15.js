6ba938024429d435aea95ec5d12d5b5a9109f37c
Edgar <edxgar48@gmail.com>
2019-08-30 17:32:56 -0300

    atualizado

---
+++ Teste_15.js
@@ -0,0 +1,27 @@
+//const dirfile_2 = 'CONCURSO_1.html'
+
+//const fs = require('fs')
+
+
+//const dirfile_1 = 'BIG_DATA.html'
+
+
+Array.prototype.forEach_2 = function(callBack){
+
+    for (let i = 0;i < this.length;i++){
+        callBack(this[i],i,this)
+    }
+
+}
+
+
+
+const arr = [0,1,2,3,4,5];
+const arr_2 = ["Agatha","Aldo","Daniel","Raquel"]
+
+
+arr_2.forEach_2(function(Nome,Indice,array){
+
+    console.log(`${Indice + 1}) ${Nome}`)
+    console.log(array)
+})
---
+++ Teste_16.js
@@ -0,0 +1,30 @@
+//const dirfile = 'd_lotfac.htm'
+const dirfile_other = 'BIG_DATA_OTHER.html'
+//const reg = /<td rowspan="\d.*">\d.*/g   // essa REGEX é MUUUITO  MELHOR
+const reg_2 = /(\<td rowspan="\d.*"\>)|(<\/td>)/g
+//const reg = /(\<td .*)/g  // essa NÂO serviu proque tem algumas linhas dentro do <td> que estão mal formatadas no original
+const fs = require('fs')
+//const replace = require('replace-in-file');
+
+const dirfile_2 = 'BIG_DATA.csv'
+//const stream_2 = fs.createWriteStream(dirfile_2,'utf8')
+//const myreadStream = fs.createReadStream(dirfile_other,'utf8')
+
+//myreadStream.on('data',(chunk_)=>{
+
+  //  var myReturn = chunk_.replace(reg_2,'') // este parte das REGEX funciona o match funciona mas NÃO o REPLACE
+
+    //  myReturn.forEach((trechoEncontrado_2)=> { stream_2.write(trechoEncontrado_2 + '\n'); });
+//})
+
+
+fs.readFile(dirfile_other, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  let result = data.replace(reg_2,'');
+
+  fs.writeFile(dirfile_2, result, 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
+});
