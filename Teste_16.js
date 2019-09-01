9fe8f7028551f080262340c4dfe905fe495fffc1
Edxgar48 <edxgar48@gmail.com>
2019-09-01 11:28:55 -0300

    alterado teste 16

--- Teste_16.js
+++ Teste_16.js
@@ -1,12 +1,12 @@
 //const dirfile = 'd_lotfac.htm'
-const dirfile_other = 'BIG_DATA_OTHER.html'
+const dirfile_2 = 'BIG_DATA_2.csv'
 //const reg = /<td rowspan="\d.*">\d.*/g   // essa REGEX é MUUUITO  MELHOR
-const reg_2 = /(\<td rowspan="\d.*"\>)|(<\/td>)/g
+//const reg_2 = /(\<td rowspan="\d.*"\>)|(<\/td>)/g
 //const reg = /(\<td .*)/g  // essa NÂO serviu proque tem algumas linhas dentro do <td> que estão mal formatadas no original
 const fs = require('fs')
 //const replace = require('replace-in-file');
 
-const dirfile_2 = 'BIG_DATA.csv'
+const dirfile_1 = 'BIG_DATA.csv'
 //const stream_2 = fs.createWriteStream(dirfile_2,'utf8')
 //const myreadStream = fs.createReadStream(dirfile_other,'utf8')
 
@@ -18,11 +18,11 @@ const dirfile_2 = 'BIG_DATA.csv'
 //})
 
 
-fs.readFile(dirfile_other, 'utf8', function (err,data) {
+fs.readFile(dirfile_1, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  let result = data.replace(reg_2,'');
+  //let result = data.replace(reg_2,'');
 
   fs.writeFile(dirfile_2, result, 'utf8', function (err) {
      if (err) return console.log(err);
